5cf4bcfbd7136c5523911b2554d98aecd13120ab
/rdndev <rdninc@mail.ru>
2024-09-05 04:03:18 +0300

    chore(ver): updated version

--- version.php
+++ version.php
@@ -28,4 +28,4 @@ $plugin->version   = 2024090500;
 $plugin->requires  = 2020061500;
 $plugin->component = 'filter_gitquotes';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '0.0.1';
+$plugin->release   = '0.0.2';
