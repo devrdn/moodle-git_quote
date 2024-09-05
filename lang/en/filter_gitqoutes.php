6a57425900c79564d5e0f5ddd5ce8fe221c6989c
/rdndev <rdninc@mail.ru>
2024-09-05 03:23:00 +0300

    fix: directory name

--- lang/en/filter_gitqoutes.php
+++ lang/en/filter_gitqoutes.php
@@ -1,4 +1,5 @@
 <?php
 
 $string['filtername'] = 'Git flavored markdown quotes filter';
+$string['pluginname'] = 'Git flavored markdown quotes filter';
 $string['pluginname_desc'] = 'This filter allows you to use Git flavored markdown quotes in your text.';
